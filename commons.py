7d4db928b7307562683f172fb328a863ac219b19
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-08-11 21:25:37 +0200

    Added feature importance calculation for all datasets

--- commons.py
+++ commons.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 from typing import List, Tuple, Union
 
 import numpy as np
@@ -6,18 +7,20 @@ from sklearn.ensemble import ExtraTreesClassifier
 
 
 def get_feature_importances(X: Union[np.ndarray, pd.DataFrame],
-                            y: Union[np.ndarray, pd.Series]) \
+                            y: Union[np.ndarray, pd.Series, pd.DataFrame]) \
                             -> List[Tuple[str, float]]:
     clf = ExtraTreesClassifier(n_estimators=500,
                                max_features=None,
                                n_jobs=-1,
                                random_state=0)
     clf.fit(X, y)
-    class_names = clf.classes_
+    feature_names = X.columns.values
     feature_importances = clf.feature_importances_
-    return sorted([(name, importance)
-                  for name, importance
-                  in zip(class_names, feature_importances)])
+    result = [(str(name), importance)
+              for name, importance
+              in zip(feature_names, feature_importances)]
+    result.sort(key=itemgetter(1), reverse=True)
+    return result
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import sklearn
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-import xgboost as xgb
+import json
+import os
+
+from commons import get_feature_importances
+import data_loaders
+
+datasets_names = ["acute_inflammations", "breast_cancer_coimbra",
+                  "breast_cancer_wisconsin"]
 
 
+def calculate_feature_importances():
+    for dataset in datasets_names:
+        loader_fun = getattr(data_loaders, "load_" + dataset)
+        X, y = loader_fun()
+        feature_importances = get_feature_importances(X, y)
+        result = dict()
+        result["all"] = feature_importances
+        result["top_3"] = feature_importances[:3]
+        result["last_3"] = feature_importances[-3:]
+        file_location = os.path.join(dataset, "feature_imporstances.json")
+        with open(file_location, "w") as file:
+            json.dump(result, file, indent=2)
 
+
+if __name__ == "__main__":
+    calculate_feature_importances()
