0aa5da392aa624dd9cbd7944848571c7a8377aa3
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-08-11 22:01:27 +0200

    Feature names for Numpy arrays are now properly handled (dummies used)

--- commons.py
+++ commons.py
@@ -9,12 +9,26 @@ from sklearn.ensemble import ExtraTreesClassifier
 def get_feature_importances(X: Union[np.ndarray, pd.DataFrame],
                             y: Union[np.ndarray, pd.Series, pd.DataFrame]) \
                             -> List[Tuple[str, float]]:
+    """
+    Calculates feature importances for a given dataset and returns them,
+    sorted descending. If names of features are not available, dummy values
+    "feature0", "feature1" etc. will be used instead.
+
+    :param X: array-like, data matrix; if it's a Pandas dataframe, the feature
+    names from it will be used
+    :param y: vector-like, class vector
+    :return: list of tuples (feature name, feature importance), sorted
+    descending by feature importances
+    """
     clf = ExtraTreesClassifier(n_estimators=500,
                                max_features=None,
                                n_jobs=-1,
                                random_state=0)
     clf.fit(X, y)
-    feature_names = X.columns.values
+    if isinstance(X, pd.DataFrame):
+        feature_names = X.columns.values
+    else:
+        feature_names = ["feature" + str(i) for i in range(X.shape[1])]
     feature_importances = clf.feature_importances_
     result = [(str(name), importance)
               for name, importance
