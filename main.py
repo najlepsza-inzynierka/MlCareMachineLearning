7a2ad9e08d4f3e31be8e0186edaffcac0b9d2712
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-08-12 17:09:37 +0200

    Hotfix for number of feature importances - less than 6 features are now properly handled

--- main.py
+++ main.py
@@ -15,8 +15,20 @@ def calculate_feature_importances():
         feature_importances = get_feature_importances(X, y)
         result = dict()
         result["all"] = feature_importances
-        result["top_3"] = feature_importances[:3]
-        result["last_3"] = feature_importances[-3:]
+        if len(feature_importances) >= 6:
+            # enough features to populate both top_3 and last_3 with empty
+            # intersection
+            result["top_3"] = feature_importances[:3]
+            result["last_3"] = feature_importances[-3:]
+        elif len(feature_importances) in {4, 5}:
+            # there are 4 or 5 features - we have enough to populate the top_3,
+            # but have to limit the last_3
+            result["top_3"] = feature_importances[:3]
+            result["last_3"] = feature_importances[3:]
+        else:
+            # there are not even enough features to fill the top_3
+            result["top_3"] = feature_importances
+            result["last_3"] = []
         file_location = os.path.join(dataset, "feature_importances.json")
         with open(file_location, "w") as file:
             json.dump(result, file, indent=2)
